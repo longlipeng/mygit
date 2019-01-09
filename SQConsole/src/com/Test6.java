b0357bec85645bd0f25b979ee2e674d7a3b506ba
v_chenming <v_chenming@chexiang.com>
2019-01-09 14:12:15 +0800

    修改

--- SQConsole/src/com/Test6.java
+++ SQConsole/src/com/Test6.java
@@ -1,24 +1,47 @@
 package com;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 public class Test6 {
 
 	public static void main(String[] args){
-		//如何打印出字符串中最长的数字段(即12987)   0-9  48-57
-		String str = "a33btsd657sd8asd80oas12987hd587nm4";
+		String loadPath = "C:/Users/名字居然用了/Pictures/Saved Pictures/d7080baf4363a8294aa26591ef695c0a.jpg";
+		String outPath = "D:/home/apache01/mchntFile/新建文件夹/123.jpg";
+		try {
+			File file1 = new File(loadPath);
+			File file2 = new File(outPath);
+			
+//			File[] fileList = file1.listFiles();
+			
+			FileInputStream in = null;
+			FileOutputStream out = null;
+			
+			in = new FileInputStream(file1);
+			out = new FileOutputStream(file2);
+	        byte[] buffer = new byte[100];// 缓存大小
+	        int readNumber = 0;
+	        while ((readNumber = in.read(buffer)) != -1) {
+	            out.write(buffer, 0, readNumber);// 读取并输出buffer数组里面0~n个字节
+	        }
 		
-		char[] charArr = str.toCharArray();
-		StringBuffer sb = new StringBuffer();
-		for (int i = 0; i < charArr.length; i++) {
-			if((int)charArr[i] >= 48 && (int)charArr[i] <= 57){
-				sb.append(charArr[i]);
-			}else{
-				sb = new StringBuffer();
-			}
+//			for (File file : fileList) {
+//				in = new FileInputStream(file);
+//				out = new FileOutputStream(file2);
+//		        byte[] buffer = new byte[100];// 缓存大小
+//		        int readNumber = 0;
+//		        while ((readNumber = in.read(buffer)) != -1) {
+//		            out.write(buffer, 0, readNumber);// 读取并输出buffer数组里面0~n个字节
+//		        }
+//			}
+	        
+	        in.close();
+	        out.close();
+		} catch (Exception e) {
+			// TODO: handle exception
 		}
-		System.out.println(sb.toString());
-		
-		
-		
 		
 	}
 }
