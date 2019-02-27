4b3f42419c379ea3658331c2e0edf60b6048a065
longlipeng <2691271388@qq.com>
2019-02-27 13:57:34 +0800

    无用项目

--- saicOnline/src/main/java/com/allinfinance/prepay/service/DepartmentService.java
+++
@@ -1,9 +0,0 @@
-package com.allinfinance.prepay.service;
-
-import com.allinfinance.prepay.exception.BizServiceException;
-import com.allinfinance.univer.entity.dto.DepartmentDTO;
-
-public interface DepartmentService {
-	public void insert(DepartmentDTO departmentDTO)throws BizServiceException;
-
-}
