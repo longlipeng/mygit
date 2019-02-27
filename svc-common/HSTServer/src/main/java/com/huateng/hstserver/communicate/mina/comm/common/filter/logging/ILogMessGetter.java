b7dbec827afab16f3fb9c22885753aadc2dc366a
longlipeng <2691271388@qq.com>
2019-02-27 14:00:13 +0800

    无用项目

--- svc-common/HSTServer/src/main/java/com/huateng/hstserver/communicate/mina/comm/common/filter/logging/ILogMessGetter.java
+++
@@ -1,7 +0,0 @@
-package com.huateng.hstserver.communicate.mina.comm.common.filter.logging;
-
-
-
-public interface ILogMessGetter {
-	Object getMessFromObj(Object message);
-}
--- svc-common/HSTServer/src/main/java/com/huateng/hstserver/communicate/mina/comm/common/filter/protocolcodec/app/IAppObjectProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.huateng.hstserver.communicate.mina.comm.common.filter.protocolcodec.app;
-
-public interface IAppObjectProcessor {
-	
-	public Object msg2obj(byte[] messageBytes) throws Exception;
-	
-	public byte[] obj2msg(Object messageObject) throws Exception;
-}
