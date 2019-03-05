fb0500bc73d7b1d67581eb8d0702e4fba409e259
longlipeng <2691271388@qq.com>
2019-03-05 16:42:17 +0800

    服务端配置中心

--- SpringCloud_ConfigServer/src/main/java/com/ConfigServerApplication.java
+++ SpringCloud_ConfigServer/src/main/java/com/ConfigServerApplication.java
@@ -1,12 +1,19 @@
 package com;
 
+//import org.springframework.boot.env.PropertiesPropertySourceLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * SpringCloud Config服务配置中心
+ * @author 名字居然用了
+ *
+ */
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
+//服务配置中心注解
 @EnableConfigServer
 public class ConfigServerApplication {
 
