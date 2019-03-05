9acd22d26c16303e1e569d08d2d9328dc7144daa
longlipeng <2691271388@qq.com>
2019-03-05 16:39:25 +0800

    客户端配置访问服务端配置中心

---
+++ SpringCloud_ConfigClient/src/main/java/com/UserController.java
@@ -0,0 +1,24 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	@Value("${user.userName}")
+	private String userName;
+	
+	@Value("${user.sex}")
+	private String sex;
+	
+	@Value("${user.age}")
+	private String age;
+	
+	@RequestMapping(value = "/hello")
+	public String hello(){
+		return "姓名:" + userName + "性别:" + sex + "年龄:" + age;
+	}
+	
+}
