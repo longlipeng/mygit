6d6240d4acd9fa471bd00177f4b33b19b59380b1
longlipeng <2691271388@qq.com>
2019-03-11 15:53:56 +0800

    添加断路器支持

--- SpringCloud_FeignClient/src/main/java/com/HelloWorldServiceFailure.java
+++ SpringCloud_FeignClient/src/main/java/com/HelloWorldServiceFailure.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 @Service("HelloWorldService")
 public class HelloWorldServiceFailure implements HelloWorldService {
 
+	/**
+	 * 错误处理方法
+	 */
 	public String sayHello() {
 		// TODO Auto-generated method stub
 		System.out.println("hello world service is not available !");
