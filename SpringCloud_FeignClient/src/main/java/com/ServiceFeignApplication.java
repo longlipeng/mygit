bbc0539c2f3836ade204d1e2ab3c69b62fa05fa6
longlipeng <2691271388@qq.com>
2019-03-11 16:25:44 +0800

    添加断路器支持

--- SpringCloud_FeignClient/src/main/java/com/ServiceFeignApplication.java
+++ SpringCloud_FeignClient/src/main/java/com/ServiceFeignApplication.java
@@ -2,12 +2,15 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 //开启服务自动发现
 @EnableDiscoveryClient
+//Feign服务由于内置断路器支持,所以不需要@EnableCircuitBreaker注解
+//@EnableCircuitBreaker
 //使用Feign
 @EnableFeignClients
 public class ServiceFeignApplication {
